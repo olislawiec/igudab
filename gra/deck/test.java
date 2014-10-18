98ce9f0f7eba6829e238a88e8072a7571b9efd98
Andrzej2275 <andrzej2275@gmail.com>
2014-10-18 13:12:42 +0000

    test class

---
+++ gra/deck/test.java
@@ -0,0 +1,75 @@
+
+public class test {
+	public static void main(String[] args)
+	{
+		byte[] deck;
+		byte[] ret=new byte[2];
+		
+		Deck d = new Deck();
+		
+		deck=d.send();
+		
+		for(int i=1;i<deck.length;i++)
+		{
+			System.out.print(deck[i]+";");
+			if (i%4==0)System.out.println("");
+		}
+		
+		d.shuffling();
+		
+		System.out.println("-----------------------------------------------");
+		
+		deck=d.send();
+		
+		for(int i=1;i<deck.length;i++)
+		{
+			System.out.print(deck[i]+";");
+			if (i%4==0)System.out.println("");
+		}
+		
+		System.out.println("-----------------------------------------------");
+		
+		byte dr=d.drow();
+		ret[0]=dr;
+		System.out.println("drow:"+dr);
+		
+		dr=d.drow();
+		ret[1]=dr;
+		System.out.println("drow:"+dr);
+			
+		deck=d.send();
+		
+		
+		for(int i=1;i<deck.length;i++)
+		{
+			System.out.print(deck[i]+";");
+			if (i%4==0)System.out.println("");
+		}
+		
+		System.out.println("-----------------------------------------------");
+		d.shuffling();
+		deck=d.send();
+		
+		for(int i=1;i<deck.length;i++)
+		{
+			System.out.print(deck[i]+";");
+			if (i%4==0)System.out.println("");
+		}
+		
+		System.out.println("-----------------------------------------------");
+		d.returnCard(ret);
+		d.shuffling();
+		
+		deck=d.send();
+		
+		for(int i=1;i<deck.length;i++)
+		{
+			System.out.print(deck[i]+";");
+			if (i%4==0)System.out.println("");
+		}
+		
+	}
+	
+	
+	
+}
