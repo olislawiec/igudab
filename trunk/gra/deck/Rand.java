ce0d8ec8c3d7de2b383f00ed915b8c6a609d4959
andrzej2275@gmail.com <andrzej2275@gmail.com@8e67ee0f-1130-427b-9d42-0afc1222f29b>
2014-10-18 12:57:57 +0000

    Class with Random generator

---
+++ trunk/gra/deck/Rand.java
@@ -0,0 +1,16 @@
+
+import java.util.Random;
+
+public class Rand {
+	   Random fate;
+	   
+	   Rand()
+	   {
+		   fate=new Random();
+	   }
+	   
+	   int random(int zakres)
+	   {
+	      return fate.nextInt(zakres);
+	   }
+}
