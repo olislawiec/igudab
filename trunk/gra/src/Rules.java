87003c4de8608702ff8d7b110994ea400d0ee617
andrzej2275@gmail.com <andrzej2275@gmail.com@8e67ee0f-1130-427b-9d42-0afc1222f29b>
2014-11-05 15:45:29 +0000

    fix //do czyszczenia

--- trunk/gra/src/Rules.java
+++ trunk/gra/src/Rules.java
@@ -1,176 +1,75 @@
 
-public class Rules {
-	
-	int[] hands;
-	int[] playerRate;
-	int players;
-	int cards=0;
-	int winner;
-	
-	Rules(int players,int[] hands)
+public class test {
+	public static void main(String[] args)
 	{
-		playerRate=new int[players+1];
+		int[] deck;
+		int[] ret=new int[2];
 		
-		for(int i=0;i<playerRate.length;i++)
+		Deck d = new Deck();
+		
+		deck=d.send();
+		
+		for(int i=0;i<deck.length;i++)
 		{
-			playerRate[i]=4;
+			System.out.print(deck[i]+";");
+			if (i%4==0)System.out.println("");
 		}
 		
-		this.players=players;
-		this.hands =hands;
-	}
-
-	private void sort()
-	{
-
-		int tempSort=0;
-		int temp=0;
-		for(int m=1;m<=this.players;m++)
+		d.shuffling();
+		
+		System.out.println("-----------------------------------------------");
+		
+		deck=d.send();
+		
+		for(int i=0;i<deck.length;i++)
 		{
-			if(m!=1)
-			{
-				temp+=4;
-			}
-
-			for (int i = 1+temp; i <= 4*m; i++) 
-			{
-	            for (int j = 1+temp; j < (4*m); j++) 
-	            {
-	                if (hands[j]%13 > hands[j + 1]%13) {
-	                    tempSort = hands[j];
-	                    hands[j] = hands[j + 1];
-	                    hands[j + 1] = tempSort;
-	                }
-	            }
-	            
-	        }
-		}
-	}
-	
-	private void handsRate()
-	{
-		int temp=0;
-		int card;
-
-		
-		for(int i=1;i<=players;i++)
-		{
-		//	card=hands[1+temp];
-			if(i!=1)temp+=4;
-			for(int j=1+temp;j<=(4*i);j++)
-			{
-				for(int m=j+1;m<=(4*i);m++)
-				{
-					if((hands[j]%13)==(hands[m]%13))
-					{
-						playerRate[i]-=1;
-						//System.out.println("powtorka-"+hands[j]+"-"+hands[m]);
-					}
-				}
-			}
-			if(this.cards<playerRate[i])
-			{
-				this.cards=playerRate[i];
-			}
-		}
-	}
-	
-	private void comp()
-	{
-		sort();
-		handsRate();
-		
-		
-		int[] player;
-		int temp=0;
-		//int j=1;
-		int v=4;
-		
-		for(int i=1;i<playerRate.length;i++)
-		{
-			if(playerRate[i]==this.cards)
-			{
-				temp++;
-				//System.out.println("dodaje do ratingu"+temp);
-			}
+			System.out.print(deck[i]+";");
+			if (i%4==0)System.out.println("");
 		}
 		
-		player=new int[temp];
-		temp=0;
-		for(int i=1;i<=this.players;i++)
-		{
-			if(playerRate[i]==this.cards)
-			{
-				//System.out.println("dodggg"+i+"-"+temp);
-				player[temp]=i;
-				//System.out.println("dodggffg"+i+"-"+player[temp]);
-				temp++;
-			}
-		}
-		//System.out.println("lol1");
+		System.out.println("-----------------------------------------------");
 		
-		int m;
-		temp=player.length;
-		while(v!=0 && temp!=0   )
-		{
+		int dr=d.drow();
+		ret[0]=dr;
+		System.out.println("drow:"+dr);
+		
+		dr=d.drow();
+		ret[1]=dr;
+		System.out.println("drow:"+dr);
 			
-			System.out.println("lol2");
-			for(int j=0;j<temp;j++)
-			{
-				System.out.println("*****j*****>"+j);
-				for(int i=0;i<temp;i++)
-				{
-					System.out.println("i>"+i+"--t->"+temp);
-				//System.out.println("ffs"+(hands[((4*player[0])+(v-4))]%13)+"-"+hands[((4*player[j])+(v-4))]%13);
-					if((hands[((4*player[j])+(v-4))]%13)>(hands[((4*player[i])+(v-4))]%13))
-					{
-						//System.out.println("");
-						System.out.println(player[j]+"-"+player[i]);
-						player[j]=player[i];
-						player[i]=player[temp-1];
-						player[temp-1]=66;	
-						temp--;
-						System.out.println(player[j]+"-"+player[i]);
-					//System.out.println(player[j]);
-					}
-					else
-					{
-						if((hands[((4*player[j])+(v-4))]%13)<(hands[((4*player[i])+(v-4))]%13))
-						{
-					/*	m=j;
-						while(m<player.length)
-						{
-						//	System.out.println("mniejsze"+m+"--"+player.length);
-							if((m+1)==temp)player[temp-1]=66;
-							else player[m]=player[m+1];
-							m++;
-						}*/
-							player[i]=player[temp-1];
-							player[temp-1]=66;	
-							temp--;
-						}
-					}
-				}
-
-
-				//System.out.println(j+"-j");
-			}
-			v--;
-
+		deck=d.send();
+		
+		
+		for(int i=0;i<deck.length;i++)
+		{
+			System.out.print(deck[i]+";");
+			if (i%4==0)System.out.println("");
 		}
-		for(int i=0;i<player.length;i++)System.out.println(player[i]);
-		winner=player[0];
+		
+		System.out.println("-----------------------------------------------");
+		d.shuffling();
+		deck=d.send();
+		
+		for(int i=0;i<deck.length;i++)
+		{
+			System.out.print(deck[i]+";");
+			if (i%4==0)System.out.println("");
+		}
+		
+		System.out.println("-----------------------------------------------");
+		d.returnCard(ret);
+		d.shuffling();
+		
+		deck=d.send();
+		
+		for(int i=0;i<deck.length;i++)
+		{
+			System.out.print(deck[i]+";");
+			if (i%4==0)System.out.println("");
+		}
+		
 	}
 	
-	public int win()
-	{
-		comp();
-		return winner;
-	}
 	
-	public int[] lol()
-	{
-		sort();
-		return hands;
-	}
+	
 }
